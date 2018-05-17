e9e4b72ef964340177ae63224129e85ea9038346
Yomguithereal <guillaumeplique@gmail.com>
2018-05-17 17:55:25 +0200

    Dumping script

--- scripts/dump_lemonde.py
+++ scripts/dump_lemonde.py
@@ -10,14 +10,43 @@ import os
 import csv
 import sys
 import mediacloud
-from pprint import pprint
+from progressbar import ProgressBar
 
 sys.path.append(os.path.join(os.getcwd()))
 from config import MEDIACLOUD_API_KEY
 
 LEMONDE_ID = 39072
+OUTPUT = './data/lemonde.csv'
 
-client = mediacloud.api.MediaCloud(MEDIACLOUD_API_KEY)
+client = mediacloud.api.AdminMediaCloud(MEDIACLOUD_API_KEY)
 
-result = client.storyList(solr_filter='media_id:%i' % LEMONDE_ID)
-pprint(result)
+nb_batches = 0
+last = 0
+with open(OUTPUT, 'w') as f:
+    writer = csv.DictWriter(f, fieldnames=['id', 'url', 'title', 'text', 'date', 'rss'])
+    writer.writeheader()
+
+    bar = ProgressBar()
+
+    while True:
+        result = client.storyList(solr_filter='media_id:%i' % LEMONDE_ID, text=1, last_processed_stories_id=last)
+
+        nb_batches += 1
+        bar.update(nb_batches)
+
+        if len(result) == 0:
+            break
+
+        for story in result:
+            writer.writerow({
+                'id': story['stories_id'],
+                'url': story['url'],
+                'title': story['title'],
+                'text': story['story_text'],
+                'date': story['publish_date'],
+                'rss': '1' if story['full_text_rss'] else '0'
+            })
+
+        last = result[-1]['stories_id']
+
+bar.finalize()

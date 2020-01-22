749d58de249da23b107c050dce4b7ad131917545
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2020-01-22 16:22:39 +0100

    add more filters to prepare_graph + remove edges before nodes

--- scripts/laroutourne/prepare_graph.py
+++ scripts/laroutourne/prepare_graph.py
@@ -6,29 +6,33 @@ import networkx as nx
 
 @click.command()
 @click.argument('filename')
-@click.option('--mindegree', default=1, type=int, show_default=True)
-@click.option('--filterfield', type=str, help="filter out nodes with attribute KEY=VAL")
+@click.option('--min-degree', default=1, type=int, show_default=True)
+@click.option('--min-indegree', default=0, type=int, show_default=True)
+@click.option('--min-outdegree', default=0, type=int, show_default=True)
+@click.option('--filter-field', type=str, help="filter out nodes with attribute KEY=VAL")
 @click.option('--weight/--no-weight', default=False, show_default=True)
-def compute(filename, mindegree, filterfield, weight):
+def compute(filename, min_degree, min_indegree, min_outdegree, filter_field, weight):
     """Prepare a GEXF network to be used with laroutourne"""
     G_in = nx.read_gexf(filename)
     G_out = nx.read_gexf(filename)
 
     filter_key = None
     filter_val = None
-    if filterfield and "=" in filterfield:
-        filter_key, filter_val = filterfield.split("=")
+    if filter_field and "=" in filter_field:
+        filter_key, filter_val = filter_field.split("=")
+
+    if not weight:
+        for src, tgt, dat in G_in.edges(data=True):
+            for key in ["weight", "count"]:
+                if key in dat:
+                    del G_out[src][tgt][key]
 
     for nid, data in G_in.nodes(data=True):
         if ((filter_key and data.get(filter_key, '') == filter_val)
-         or (mindegree and G_in.degree(nid) < mindegree)):
+         or (min_degree and G_in.degree(nid) < min_degree)
+         or (min_indegree and G_in.in_degree(nid) < min_indegree)
+         or (min_outdegree and G_in.out_degree(nid) < min_outdegree)):
             G_out.remove_node(nid)
-
-    if not weight:
-        for e in G_in.edges(data=True):
-            for key in ["weight", "count"]:
-                if key in e[2]:
-                    del(G_out[e[0]][e[1]][key])
 
     nx.write_gexf(G_out, filename.replace(".gexf", "-prepared.gexf"))
     nx.write_graphml(G_out, filename.replace(".gexf", "-prepared.graphml"))

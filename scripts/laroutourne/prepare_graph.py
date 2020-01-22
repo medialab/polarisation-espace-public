ac4858f4705aa7d94a99a6c6da8f7cf27c5f3f52
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2020-01-22 15:28:41 +0100

    better graph clean script

--- scripts/laroutourne/prepare_graph.py
+++ scripts/laroutourne/prepare_graph.py
@@ -1,27 +1,38 @@
 #!/usr/bin/env python
 
-import sys
+import click
 import networkx as nx
 
-filename = sys.argv[1]
 
-G_in = nx.read_gexf(filename)
-G_out = nx.read_gexf(filename)
+@click.command()
+@click.argument('filename')
+@click.option('--mindegree', default=1, type=int, show_default=True)
+@click.option('--filterfield', type=str, help="filter out nodes with attribute KEY=VAL")
+@click.option('--weight/--no-weight', default=False, show_default=True)
+def compute(filename, mindegree, filterfield, weight):
+    """Prepare a GEXF network to be used with laroutourne"""
+    G_in = nx.read_gexf(filename)
+    G_out = nx.read_gexf(filename)
 
-# TODO add CLI option to filter nodes with cat=val attributes
-#for nid, data in G.nodes(data="batch"):
-#    if data['batch'] == "EU":
-#        G.remove_node(nid)
+    filter_key = None
+    filter_val = None
+    if filterfield and "=" in filterfield:
+        filter_key, filter_val = filterfield.split("=")
 
-# TODO add CLI option to filter nodes with indegree lower than threshold
-#for nid, data in G.nodes(data="batch"):
-#    if G.in_degree(nid) < 3:
-#        G.remove_node(nid)
-#        print(nid, data)
+    for nid, data in G_in.nodes(data=True):
+        if ((filter_key and data.get(filter_key, '') == filter_val)
+         or (mindegree and G_in.degree(nid) < mindegree)):
+            G_out.remove_node(nid)
 
-for e in G_in.edges(data=True):
-    for key in ["weight", "count"]:
-        if key in e[2]:
-            del(G_out[e[0]][e[1]][key])
+    if not weight:
+        for e in G_in.edges(data=True):
+            for key in ["weight", "count"]:
+                if key in e[2]:
+                    del(G_out[e[0]][e[1]][key])
 
-nx.write_graphml(G_out, filename.replace(".gexf", ".graphml"))
+    nx.write_gexf(G_out, filename.replace(".gexf", "-prepared.gexf"))
+    nx.write_graphml(G_out, filename.replace(".gexf", "-prepared.graphml"))
+
+
+if __name__ == '__main__':
+    compute()

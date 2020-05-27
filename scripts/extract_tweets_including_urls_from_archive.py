d313ec9060df37804ee879de04b01b70a35ee638
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2020-05-27 18:34:40 +0200

    add exporting from mongo alternative

--- scripts/extract_tweets_including_urls_from_archive.py
+++ scripts/extract_tweets_including_urls_from_archive.py
@@ -2,10 +2,13 @@ import sys
 import csv
 import gzip
 from collections import defaultdict
+from pymongo import MongoClient
 
 import casanova
 from tqdm import tqdm
 from ural import normalize_url
+
+from tweets_metas import get_field
 
 
 # Read list urls to filter
@@ -18,9 +21,49 @@ def read_list_urls(list_urls_file):
     return categories_medias_urls
 
 
-def filter_and_enrich_tweets(f, cat_urls, of=sys.stdout, total=None):
+def filter_and_enrich_tweets_from_mongo(db, cat_urls, of=sys.stdout):
     categories = list(cat_urls.keys())
-    casa = casanova.enricher(f, of, add=["webentities"] + categories)
+    fields = "id,time,created_at,from_user_name,text,filter_level,possibly_sensitive,withheld_copyright,withheld_scope,withheld_countries,truncated,retweet_count,favorite_count,reply_count,lang,to_user_name,to_user_id,in_reply_to_status_id,source,source_name,source_url,location,lat,lng,from_user_id,from_user_realname,from_user_verified,from_user_description,from_user_url,from_user_profile_image_url,from_user_utcoffset,from_user_timezone,from_user_lang,from_user_tweetcount,from_user_followercount,from_user_friendcount,from_user_favourites_count,from_user_listed,from_user_withheld_scope,from_user_withheld_countries,from_user_created_at,collected_via_search,collected_via_stream,collected_via_thread_only,collected_at_timestamp,retweeted_id,retweeted_user_name,retweeted_user_id,quoted_id,quoted_user_name,quoted_user_id,links,medias_urls,medias_files,mentioned_user_names,mentioned_user_ids,hashtags".split(",")
+    headers = fields + ["matched_urls", "webentities"] + categories
+    writer = csv.DictWriter(of, fieldnames=headers, extrasaction="ignore")
+    writer.writeheader()
+
+    for t in tqdm(db.find(), total=db.count()):
+        if len(t.keys()) < 10:
+            continue
+        for f in fields:
+            t[f] = get_field(f, t)
+        try:
+            links = [normalize_url(u) for u in t["links"].split('|')]
+            if not links:
+                continue
+
+            t["matched_urls"] = []
+            t["webentities"] = set()
+            for cat in categories:
+                cat_match = False
+                for we, urls in cat_urls[cat].items():
+                    for u in links:
+                        if u in urls:
+                            cat_match = True
+                            t["matched_urls"].append(u)
+                            t["webentities"].add(we)
+                            links.remove(u)
+                t[cat] = cat_match
+
+            if t["webentities"]:
+                t["matched_urls"] = "|".join(t["matched_urls"])
+                t["webentities"] = "|".join(t["webentities"])
+                writer.writerow(t)
+
+        except Exception as e:
+            print("ERROR while processing", t, file=sys.stderr)
+            raise(e)
+
+
+def filter_and_enrich_tweets_from_csv(f, cat_urls, of=sys.stdout, total=None):
+    categories = list(cat_urls.keys())
+    casa = casanova.enricher(f, of, add=["matched_urls", "webentities"] + categories)
     links_pos = casa.pos.links
 
     for row in tqdm(casa, total=total):
@@ -29,7 +72,8 @@ def filter_and_enrich_tweets(f, cat_urls, of=sys.stdout, total=None):
             if not links:
                 continue
 
-            webentities = []
+            matched_urls = []
+            webentities = set()
             cat_belongings = []
             for cat in categories:
                 cat_match = False
@@ -37,13 +81,13 @@ def filter_and_enrich_tweets(f, cat_urls, of=sys.stdout, total=None):
                     for u in links:
                         if u in urls:
                             cat_match = True
-                            webentities.append(we)
+                            matched_urls.append(u)
+                            webentities.add(we)
                             links.remove(u)
-                            break
                 cat_belongings.append(cat_match)
 
             if webentities:
-                casa.writerow(row, ["|".join(webentities)] + cat_belongings)
+                casa.writerow(row, ["|".join(matched_urls), "|".join(webentities)] + cat_belongings)
 
         except Exception as e:
             print("ERROR while processing", row, file=sys.stderr)
@@ -56,13 +100,19 @@ def gzip_open(filename):
 
 if __name__ == "__main__":
     list_urls_file = sys.argv[1]
-    csv_archive = sys.argv[2]
+    source = sys.argv[2]
     csv_lines = int(sys.argv[3]) if len(sys.argv) > 3 else None
 
     cat_urls = read_list_urls(list_urls_file)
 
-    # read tweets from tweets archive (csv possibly gzipped)
-    open_wrapper = gzip_open if csv_archive.endswith(".gz") else open
-    with open_wrapper(csv_archive) as f:
-        setattr(sys.stdout, 'tell', lambda: 0)
-        filter_and_enrich_tweets(f, cat_urls, total=csv_lines)
+    if source.endswith(".csv") or source.endswith(".gz"):
+        # read tweets from tweets archive (csv possibly gzipped)
+        open_wrapper = gzip_open if source.endswith(".gz") else open
+        with open_wrapper(source) as f:
+            setattr(sys.stdout, 'tell', lambda: 0)
+            filter_and_enrich_tweets_from_csv(f, cat_urls, total=csv_lines)
+
+    else:
+        # read tweets from mongodb
+        mongodb = MongoClient('localhost', 27017)[source]['tweets']
+        filter_and_enrich_tweets_from_mongo(mongodb, cat_urls)

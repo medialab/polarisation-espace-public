ecda0af1a7094a708a6f6dadd1a49d085d2e7715
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2021-04-30 16:24:25 +0200

    prepare possibility to filter lang and min date

--- scripts/extract_users_urls_from_tweets_archive.py
+++ scripts/extract_users_urls_from_tweets_archive.py
@@ -25,7 +25,7 @@ def build_medias_trie():
 
 
 
-def extract_users_urls_medias_from_csv(f, trie, of=sys.stdout, total=None):
+def extract_users_urls_medias_from_csv(f, trie, of=sys.stdout, total=None, filter_fr=False, min_date=None):
     headers = ['tweet_id', 'user_screen_name', 'user_id',
                'normalized_url', 'domain_name', 'webentity',
                'datetime', 'is_retweet', 'nb_followers']
@@ -33,10 +33,14 @@ def extract_users_urls_medias_from_csv(f, trie, of=sys.stdout, total=None):
     writer.writerow(headers)
     casa = casanova.reader(f)
     try:
-        for row, (tid, uname, uid, dtime, rtid, nbfols, links) in tqdm(enumerate(
+        for row, (tid, uname, uid, dtime, rtid, nbfols, links, lang) in tqdm(enumerate(
           casa.cells(['id', 'from_user_name', 'from_user_id', 'created_at',
-                      'retweeted_id', 'from_user_followercount', 'links'])),
+                      'retweeted_id', 'from_user_followercount', 'links', 'lang'])),
           total=total):
+            if filter_fr and lang != 'fr':
+                continue
+            if min_date and dtime < min_date:
+                continue
             is_rt = (rtid != '')
             for url in links.split('|'):
                 url = url.strip()
@@ -61,7 +65,20 @@ def gzip_open(filename):
 
 
 if __name__ == "__main__":
+
+    filter_fr = False
+    if "--fr" in sys.argv:
+        filter_fr = True
+        sys.argv.remove("--fr")
+
+    min_date = None
+    if "--min-date" in sys.argv:
+        min_date = sys.argv[sys.argv.index('--min-date') + 1]
+        sys.argv.remove("--min-date")
+        sys.argv.remove(min_date)
+
     source = sys.argv[1]
+
     csv_lines = int(sys.argv[2]) if len(sys.argv) > 2 else None
 
     trie = build_medias_trie()
@@ -70,4 +87,4 @@ if __name__ == "__main__":
     open_wrapper = gzip_open if source.endswith(".gz") else open
     with open_wrapper(source) as f:
         setattr(sys.stdout, 'tell', lambda: 0)
-        extract_users_urls_medias_from_csv(f, trie, total=csv_lines)
+        extract_users_urls_medias_from_csv(f, trie, total=csv_lines, filter_fr=filter_fr, min_date=None)
